e7e4289f75878d3a1a36a45c727032b5471b43d1
Adam Jolicoeur <adam.jolicoeur@redhat.com>
2017-12-11 14:37:42 -0500

    maintenance

--- js/site.js
+++ js/site.js
@@ -1,3 +1,15 @@
+// jQuery to collapse the navbar on scroll
+function collapseNavbar() {
+  if ($(".navbar").offset().top > 50) {
+    $(".fixed-top").addClass("top-nav-collapse");
+  } else {
+    $(".fixed-top").removeClass("top-nav-collapse");
+  }
+}
+
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
+
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function () {
   $('a.page-scroll').bind('click', function (event) {
